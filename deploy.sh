8628fbed5ad6850d954f4f05dc7959291c8454ed
Felix Becker <felix.b@outlook.com>
2018-09-01 00:43:14 +0200

    Push to DockerHub

--- deploy.sh
+++ deploy.sh
@@ -13,12 +13,12 @@ npm prune --production
 
 # Build image
 VERSION=$(printf "%05d" $BUILDKITE_BUILD_NUMBER)_$(date +%Y-%m-%d)_$(git rev-parse --short HEAD)
-docker build -t us.gcr.io/sourcegraph-dev/search-badger:$VERSION .
+docker build -t sourcegraph/search-badger:$VERSION .
 
 # Upload
-gcloud docker -- push us.gcr.io/sourcegraph-dev/search-badger:$VERSION
-docker tag us.gcr.io/sourcegraph-dev/search-badger:$VERSION us.gcr.io/sourcegraph-dev/search-badger:latest
-gcloud docker -- push us.gcr.io/sourcegraph-dev/search-badger:latest
+gcloud docker -- push sourcegraph/search-badger:$VERSION
+docker tag sourcegraph/search-badger:$VERSION sourcegraph/search-badger:latest
+gcloud docker -- push sourcegraph/search-badger:latest
 
 # Trigger Deploybot (branch is just a unique identifier)
 curl http://deploy-bot.sourcegraph.com/set-branch-version -F "token=$DEPLOY_BOT_TOKEN" -F "branch=search-badger" -F "version=$VERSION" -F "user=$BUILDKITE_BUILD_CREATOR_EMAIL"
